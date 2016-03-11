fc5ea872e6d874445faa4f7b3f1dbce5bb66c1b4
Milan Iliev <milaniliev@gmail.com>
2016-03-11 01:49:18 -0500

    Initial version

---
+++ app.js
@@ -0,0 +1,57 @@
+var buildings = [
+  {name: "Calkins Hall", places: [
+    {name: "Calkins Lab", resources: ["Student Computing Services"]}
+  ]},
+  {name: "Adams Hall", places: [
+    {name: "Innovation lab"}, {name: "Linux lab"},
+    {name: "Computer Science Department Office",  resources: ["Department Chair"]}
+  ]}
+]
+
+var places_list = document.querySelector('[id=places_list]')
+
+buildings.forEach(function(building) {
+  building.element = document.createElement('div')
+  building.element.textContent = building.name
+  building.places.forEach(function(place){
+    place.element = document.createElement('div')
+    place.element.textContent = place.name
+    place.element.classList.add('place')
+
+    building.element.appendChild(place.element)
+  })
+  
+  building.element.classList.add('building')
+  places_list.appendChild(building.element)
+})
+
+var search_field = document.querySelector("[id=search_field]")
+
+search_field.addEventListener('keyup', function(){
+  buildings.forEach(function(building){
+    var building_name_matches = building.name.match(new RegExp(search_field.value, 'i'))
+    var place_matched = false
+     
+    building.places.forEach(function(place){
+      var place_name_matches = place.name.match(new RegExp(search_field.value, 'i'))
+      if(place_name_matches)
+      {
+        place_matched = true
+        place.element.style.display = ''
+      }
+      else
+        {
+          place.element.style.display = 'none'
+        }
+    })
+ 
+    
+    if(building_name_matches || place_matched){
+       building.element.style.display = ''
+    } else {
+       building.element.style.display = 'none'
+    }
+  })
+  
+})
+
